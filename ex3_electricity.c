14b43c020c5eb0f68aa306c9fb3f4f5cba1f501f
sammyjoashink-tech <sammyjoashink@gmail.com>
2026-03-24 20:00:09 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,35 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Kasozi Joashin Sammy */
+/* Student Number: 25/U/BIE/01379/PE */
 
 #include <stdio.h>
+int main(){
+    float units, bill_amount, cost_per_unit;
+    printf("enter the number of units consumed:");
+    scanf("%f", &units);
+    printf("enter the cost per unit:");
+    scanf("%f", &cost_per_unit);
 
-int main(void)
-{
-    /* Write your code here */
+    if (units <= 100){
+        bill_amount = 0.5 * units * cost_per_unit;
+        printf("the bill amount is: %f\n", bill_amount);
 
+    }
+
+    else if (units > 100 && units <= 200){
+        bill_amount = 0.75 * units * cost_per_unit;
+        printf("the bill amount is: %f\n", bill_amount);
+    }
+
+    else if (units > 200 && units <= 300){
+        bill_amount = 1.20 * units * cost_per_unit;
+        printf("the bill amount is: %f\n", bill_amount);
+
+    }
+
+    else if (units > 300){
+        bill_amount = 1.50 * units *cost_per_unit;
+        printf("the bill amount is: %f\n", bill_amount);
+    }
     return 0;
-}
+        
+    }
