86dcedc610c38506856632688e54b2554961aee5
sammyjoashink-tech <sammyjoashink@gmail.com>
2026-03-24 19:42:43 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,31 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Kasozi Joashin Sammy*/
+/* Student Number: 25/U/BIE/01379/PE */
 
 #include <stdio.h>
+int main(){
+    float amount, fee ;
+    printf("enter the amount to be sent:");
+    scanf("%f", &amount);
 
-int main(void)
-{
-    /* Write your code here */
-
+    if (amount <= 100){
+        fee = 0.05 * amount;
+        printf("the fee is: %f\n", fee);
+    }
+        else if (amount<= 2500){
+            fee = 0.04 * amount;
+            printf("the fee is: %f\n", fee);
+        }
+        else if (amount <= 10000){
+            fee = 0.03 * amount;
+            printf("the fee is: %f\n", fee);
+        }
+        else if (amount <= 30000){ 
+            fee = 0.02 * amount;
+            printf("the fee is: %f\n", fee);
+        }
+        else if (amount > 60000){
+            fee = 0.01 * amount;
+            printf("the fee is: %f\n", fee);
+        }
     return 0;
 }
